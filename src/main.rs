257cea69c13f494bdd32a2a959fba49990bef0f5
GoogleFeud <volen.sl666@gmail.com>
2022-04-08 23:03:54 +0300

    Add a cli

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,34 @@ mod detectors;
 pub use analyzer::{Analyzer};
 pub use detectors::amongi::AmongiDetector;
 use std::time::{Instant};
+use argh::FromArgs;
 
+#[derive(FromArgs)]
+/// Detect among us characters
+struct Args {
+    #[argh(option)]
+    /// the image
+    pub image: String,
+    #[argh(option)]
+    /// shows time
+    pub time: Option<bool>,
+    #[argh(option)]
+    /// where to store the final result
+    pub out: Option<String>
+}
 
 fn main() {
-    let analyzer = Analyzer::from("./place.png").expect("Image doesn't exist.");
+    let args = argh::from_env::<Args>();
+    let analyzer = Analyzer::from(&args.image).expect("Image doesn't exist.");
     let mut amongi_collector = AmongiDetector::new();
     let before = Instant::now();
     let pixels = analyzer.run(vec![&mut amongi_collector]);
-    analyzer.highlight(pixels).save_with_format("./result.png", image::ImageFormat::Png).expect("Couldn't save image");
+    analyzer.highlight(pixels).save_with_format(args.out.unwrap_or("./result.png".to_string()), image::ImageFormat::Png).expect("Couldn't save image.");
     let total_amongi = amongi_collector.results.values().sum::<u32>();
     println!("Found {} amongi", total_amongi);
-    println!("Took {} seconds", before.elapsed().as_secs_f32());
+    if let Some(show_time) = args.time {
+        if show_time {
+            println!("Took {} seconds", before.elapsed().as_secs_f32());
+        }
+    }
 }
